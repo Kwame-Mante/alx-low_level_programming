0bf136c550c9c9789232c51b99ad3aaf2e47d20e
Kwame-Mante <nanakwame008@gmail.com>
2023-02-20 05:04:58 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all combinations of single digits
+ * Return: Always 0
+*/
+int main(void)
+{
+int p;
+for (p = 48; p < 58; p++)
+{
+putchar(p);
+if (p != 57)
+{
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
