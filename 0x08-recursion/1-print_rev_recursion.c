543730b1efafe5c03984b69d2e47bf9086f4dbc1
Kwame-Mante <nanakwame008@gmail.com>
2023-03-09 04:20:23 -0800

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -print_rev_recursion-prints a string in reverse
+ * _print_rev_recursion-prints a string in reverse
  *  @s: the string to be printed
  */
 void _print_rev_recursion(char *s)
